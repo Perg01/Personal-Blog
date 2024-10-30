2c52a46749ce0d5cf8984c8d90f92d3fb7a97c9d
Perg01 <jsatshs@gmail.com>
2024-10-30 16:40:44 -0700

    Add delete, home page, read article functionality

--- app.js
+++ app.js
@@ -57,15 +57,19 @@ function editArticle(id, updatedData) {
 
     return new Promise((resolve, reject) => {
 
+        // Reading data from file
         fs.readFile(filePath, 'utf-8', (readErr, data) => {
 
             if (readErr) {
                 return reject(readErr);
             }
 
+            // Take JSON data from file and convert it to object
             const articleData = JSON.parse(data);
+            // Merging the current article data with the updated data
             const articleToSave = { ...articleData, ...updatedData, id };
 
+            // Writing the updated data to file
             fs.writeFile(filePath, JSON.stringify(articleToSave, null, 2), (writeErr) => {
                 if (writeErr) {
                     reject(writeErr);
@@ -107,6 +111,43 @@ function getAllArticles() {
 
     return articles;
 }
+
+// Delete article
+function deleteArticle(id) {
+    fs.unlinkSync(path.join(articlesPath, `${id}.json`));
+}
+
+// Displays articles on home page
+app.get('/home', (req, res) => {
+    try {
+        const articles = getAllArticles();
+        res.render('home.ejs', { articles });
+    } catch (err) {
+        res.status(500).send('Something went wrong, can\'t fetch all articles');
+    }
+});
+
+// Route to delete article
+app.post('/admin/delete/:id', async (req, res) => {
+    try {
+        deleteArticle(req.params.id);
+        res.redirect('/admin');
+    } catch (err) {
+        console.error(`Failed to delete article: ${err}`);
+        res.status(500).send('Something went wrong, can\'t delete article');
+    }
+});
+
+// Route to load the article page
+app.get('/article/:id', async (req, res) => {
+    try {
+        const { id } = req.params;
+        const article = await getArticle(id);
+        res.render('article.ejs', { article });
+    } catch (err) {
+        res.status(500).send('Something went wrong, can\'t fetch article');
+    }
+});
 
 // Route to fetch article
 app.get('/edit/:id', async (req, res) => {
